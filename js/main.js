6d614f94c6cb415e1f9d1016b5a68fee3dc854ae
Ryan Bae <baeryan21@gmail.com>
2022-04-05 07:21:57 +0000

    Listened for input events on photoUrl input. #1

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
 /* global data */
 /* exported data */
+var placeHolderImage = document.querySelector('.placeholder');
+var photoUrl = document.querySelector('.photourl');
+
+photoUrl.addEventListener('input', imageEvent);
+
+function imageEvent(event) {
+  var photo = event.target.value;
+  placeHolderImage.setAttribute('src', photo);
+}
