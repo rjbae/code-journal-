a04154bc998b870eb094a071ae76f46f4b2266da
Ryan Bae <baeryan21@gmail.com>
2022-04-05 19:29:47 +0000

    Verified that new entries are saved to local storage. #1

--- js/main.js
+++ js/main.js
@@ -1,11 +1,27 @@
 /* global data */
 /* exported data */
-var placeHolderImage = document.querySelector('.placeholder');
+var placeHolderImg = document.querySelector('.placeholder');
 var photoUrl = document.querySelector('.photourl');
 
 photoUrl.addEventListener('input', imageEvent);
 
 function imageEvent(event) {
   var photo = event.target.value;
-  placeHolderImage.setAttribute('src', photo);
+  placeHolderImg.setAttribute('src', photo);
+}
+
+var form = document.querySelector('.form');
+form.addEventListener('submit', submitEvent);
+
+function submitEvent(event) {
+  event.preventDefault();
+  var formEntry = {};
+  formEntry.title = form.elements.title.value;
+  formEntry.photoUrl = form.elements.photoUrl.value;
+  formEntry.notes = form.elements.notes.value;
+  formEntry.entryId = data.nextEntryId;
+  data.nextEntryId++;
+  data.entries.unshift(formEntry);
+  placeHolderImg.setAttribute('src', 'images/placeholder-image-square.jpg');
+  form.reset();
 }
