d724e3df40faea2944ab2e411a0e815f5cfd7236
Ryan Bae <baeryan21@gmail.com>
2022-04-06 21:39:11 +0000

    Created a DOM tree and loop for each journal entry and removed example HTML entries once the entries were appended to the DOM on reload.

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ function imageEvent(event) {
 var form = document.querySelector('.form');
 form.addEventListener('submit', submitEvent);
 
-function submitEvent(event) {
+function submitEvent(entry) {
   event.preventDefault();
   var formEntry = {};
   formEntry.title = form.elements.title.value;
@@ -24,4 +24,48 @@ function submitEvent(event) {
   data.entries.unshift(formEntry);
   placeHolderImg.setAttribute('src', 'images/placeholder-image-square.jpg');
   form.reset();
+}
+
+function renderEntry(entry) {
+
+  var list = document.createElement('li');
+
+  var row = document.createElement('div');
+  row.setAttribute('class', 'row');
+  list.appendChild(row);
+
+  var columnHalf = document.createElement('div');
+  columnHalf.setAttribute('class', 'column-half');
+  row.appendChild(columnHalf);
+
+  var image = document.createElement('img');
+  image.setAttribute('class', 'entry-img');
+  image.setAttribute('src', entry.photoUrl);
+  columnHalf.appendChild(image);
+
+  var secondColumn = document.createElement('div');
+  secondColumn.setAttribute('class', 'column-half');
+  row.appendChild(secondColumn);
+
+  var entryHeader = document.createElement('h3');
+  var entryHeaderText = document.createTextNode(entry.title);
+  entryHeader.setAttribute('class', 'entry-header');
+  secondColumn.appendChild(entryHeader);
+  entryHeader.appendChild(entryHeaderText);
+
+  var entryParagraph = document.createElement('p');
+  var entryText = document.createTextNode(entry.notes);
+  secondColumn.appendChild(entryParagraph);
+  entryParagraph.appendChild(entryText);
+  return list;
+}
+
+var list = document.querySelector('ul');
+document.addEventListener('DOMContentLoaded', DOMContentLoaded);
+
+function DOMContentLoaded(event) {
+  for (var i = 0; i < data.entries.length; i++) {
+    var entry = renderEntry(data.entries[i]);
+    list.appendChild(entry);
+  }
 }
